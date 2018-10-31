c1f42661b15467fb59434c02e90f9be916f857b0
hhmings <1574762855@qq.com>
2018-10-31 21:42:50 +0800

    Initial commit

---
+++ src/main/java/com/hhm/mygit/MygitApplication.java
@@ -0,0 +1,12 @@
+package com.hhm.mygit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygitApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hhm/mygit/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.hhm.mygit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ming
+ * @create 2018-10-31 21:32
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "ok";
+    }
+
+}
